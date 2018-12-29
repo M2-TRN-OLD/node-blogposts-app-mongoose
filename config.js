1d5d23230c795065d20da469f9df0c6e934cd052
Missy Maloney <missymaloney1@gmail.com>
2018-12-29 10:25:30 -0500

    changed db name to posts

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
 exports.DATABASE_URL = 
-  process.env.DATABASE_URL || 'mongodb://localhost/node-blogposts-app-mongoose/data';
+  process.env.DATABASE_URL || 'mongodb://localhost/node-blogposts-app-mongoose/posts';
 exports.PORT = process.env.PORT || 8080;
